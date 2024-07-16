511167726958cf6ca94324ff2dea6f037ec5cd1d
SeungYeon04 <epsl.kwm@gmail.com>
2024-07-16 16:02:06 +0900

    fork

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Container from './page/ask/Container';
+import "./App.css";
+import Container from "./page/ask/Container";
+import Container_v2 from "./page/ask/Container_v2";
+import Container_v3 from "./page/ask/Container_v3";
 
 function App() {
   return (
     <div className="App">
-      <Container/> 
+      <Container></Container>
+      <Container_v2></Container_v2>
+      <Container_v3></Container_v3>
     </div>
   );
 }
